4cb558f571a0f17d4bd945e5b2c5f0aabb6b9d52
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-11-15 13:23:46 +0100

    quarkus init for auth service

---
+++ auth-service/src/test/java/cz/muni/fi/pv217/prociv/auth/service/NativeAuthenticationResourceIT.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pv217.prociv.auth.service;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAuthenticationResourceIT extends AuthenticationResourceTest {
+
+    // Execute the same tests but in native mode.
+}
