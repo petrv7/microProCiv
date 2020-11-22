dfbbb645e7be6328d32494343856d00d6278de34
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-11-22 16:39:11 +0100

    added some metrics

--- auth-service/src/main/java/cz/muni/fi/pv217/prociv/auth/service/AuthenticationResource.java
+++ auth-service/src/main/java/cz/muni/fi/pv217/prociv/auth/service/AuthenticationResource.java
@@ -5,6 +5,9 @@ import cz.muni.fi.pv217.prociv.auth.service.exceptions.AuthException;
 import cz.muni.fi.pv217.prociv.auth.service.services.AuthenticationService;
 import cz.muni.fi.pv217.prociv.auth.service.services.TokenService;
 import io.smallrye.jwt.build.Jwt;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -28,6 +31,7 @@ public class AuthenticationResource {
     @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Counted(name = "numberOfLogins", description = "How many logins have been performed.")
     public String login(AuthData data) throws AuthException {
         try {
             if (!authService.loginUser(data.username, data.password)) {
@@ -43,6 +47,8 @@ public class AuthenticationResource {
     @POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Counted(name = "numberOfRegistrations", description = "How many registrations have been performed.")
+    @Timed(name = "registerationTimer", description = "A measure of how long it takes to register a new user.", unit = MetricUnits.MILLISECONDS)
     public String register(AuthData data) throws AuthException {
         try {
             authService.registerUser(data.username, data.password);
