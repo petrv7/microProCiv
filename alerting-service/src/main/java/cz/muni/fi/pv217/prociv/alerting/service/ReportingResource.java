f66c09855dddf6e7bcc257474364816c06d2ca9b
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-11-30 13:04:11 +0100

    reporting resource for alerting service

---
+++ alerting-service/src/main/java/cz/muni/fi/pv217/prociv/alerting/service/ReportingResource.java
@@ -0,0 +1,59 @@
+package cz.muni.fi.pv217.prociv.alerting.service;
+
+import cz.muni.fi.pv217.prociv.alerting.service.data.Alert;
+import cz.muni.fi.pv217.prociv.alerting.service.data.Location;
+import cz.muni.fi.pv217.prociv.alerting.service.data.Report;
+import cz.muni.fi.pv217.prociv.alerting.service.data.ReportFilterOptions;
+import cz.muni.fi.pv217.prociv.alerting.service.exceptions.AlertException;
+import cz.muni.fi.pv217.prociv.alerting.service.services.ReportingService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.util.Date;
+import java.util.List;
+
+@Path("/reports")
+public class ReportingResource {
+
+    @Inject
+    private ReportingService reportingService;
+
+    @POST
+    @Path("/new")
+    @RolesAllowed("User")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String newReport(Report report, @Context SecurityContext ctx) {
+        report.username = ctx.getUserPrincipal().getName();
+        reportingService.addReport(report);
+        return "Report successfully added!";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Report> getAllReports() {
+        return reportingService.getAllReports();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Report getReport(@PathParam("id") Long id) throws AlertException {
+        try {
+            return reportingService.getReport(id);
+        } catch (Exception e) {
+            throw new AlertException(e.getMessage());
+        }
+    }
+
+    @GET
+    @Path("/filter")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Report> getFilteredReports(ReportFilterOptions options) {
+        return reportingService.getFilteredReports(options.date, options.location);
+    }
+}
