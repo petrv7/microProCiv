ed349e469fc7c3ca19e3114cb19ea63079abcaa0
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-12-06 12:37:48 +0100

    alerting service detalied openapi

--- alerting-service/src/main/java/cz/muni/fi/pv217/prociv/alerting/service/AlertingResource.java
+++ alerting-service/src/main/java/cz/muni/fi/pv217/prociv/alerting/service/AlertingResource.java
@@ -7,6 +7,9 @@ import cz.muni.fi.pv217.prociv.alerting.service.services.AlertingService;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -26,6 +29,10 @@ public class AlertingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Counted(name = "numberOfNewAlerts", description = "How many new alerts have been added.")
     @Timed(name = "newAlertTimer", description = "A measure of how long it takes to add a new alert.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Add a new alert. Requires admin JWT authorization token.")
+    @RequestBody(description = "Alert info")
+    @APIResponse(responseCode = "200", description = "Successfully added new alert.")
+    @APIResponse(responseCode = "401", description = "Unauthorized access.")
     public String newAlert(Alert alert) throws AlertException {
         try {
             alertingService.addAlert(alert);
@@ -38,6 +45,8 @@ public class AlertingResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Find alert with specific ID.")
+    @APIResponse(responseCode = "200", description = "Alert with given ID found.")
     public Alert getAlert(@PathParam("id") Long id) throws AlertException {
         try {
             return alertingService.getAlert(id);
@@ -50,6 +59,8 @@ public class AlertingResource {
     @Path("/location/{loc}")
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = "getAlertsByLocationTimer", description = "A measure of how long it takes to filter alerts based on location.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Find all alerts in a specific location.")
+    @APIResponse(responseCode = "200", description = "List of all alerts in a specific location.")
     public List<Alert> getAlertsByLocation(@PathParam("loc") Location loc) {
         return alertingService.getAlertsByLocation(loc, false);
     }
@@ -58,12 +69,16 @@ public class AlertingResource {
     @Path("/location/{loc}/active")
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = "getActiveAlertsByLocationTimer", description = "A measure of how long it takes to filter active alerts based on location.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Find active alerts in a specific location.")
+    @APIResponse(responseCode = "200", description = "List of active alerts in a specific location.")
     public List<Alert> getActiveAlertsByLocation(@PathParam("loc") Location loc) {
         return alertingService.getAlertsByLocation(loc, true);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Find all alerts.")
+    @APIResponse(responseCode = "200", description = "List of all alerts.")
     public List<Alert> getAllAlerts() {
         return alertingService.getAllAlerts(false);
     }
@@ -71,6 +86,8 @@ public class AlertingResource {
     @GET
     @Path("/active")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Find active alerts.")
+    @APIResponse(responseCode = "200", description = "List of active alerts.")
     public List<Alert> getAllActiveAlerts() {
         return alertingService.getAllAlerts(true);
     }
@@ -79,6 +96,9 @@ public class AlertingResource {
     @RolesAllowed("Admin")
     @Path("/activate/{id}")
     @Counted(name = "numberOfActivateAlerts", description = "How many times alerts have been activated.")
+    @Operation(summary = "Activate alert with specific ID. Requires admin JWT authorization token.")
+    @APIResponse(responseCode = "200", description = "Alert successfully activated.")
+    @APIResponse(responseCode = "401", description = "Unauthorized access.")
     public String activateAlert(@PathParam("id") Long id) throws AlertException {
         try {
             alertingService.activateAlert(id);
@@ -92,6 +112,9 @@ public class AlertingResource {
     @RolesAllowed("Admin")
     @Path("/deactivate/{id}")
     @Counted(name = "numberOfDeactivateAlerts", description = "How many times alerts have been deactivated.")
+    @Operation(summary = "Deactivate alert with specific ID. Requires admin JWT authorization token.")
+    @APIResponse(responseCode = "200", description = "Alert successfully deactivated.")
+    @APIResponse(responseCode = "401", description = "Unauthorized access.")
     public String deactivateAlert(@PathParam("id") Long id) throws AlertException {
         try {
             alertingService.deactivateAlert(id);
--- alerting-service/src/main/java/cz/muni/fi/pv217/prociv/alerting/service/ReportingResource.java
+++ alerting-service/src/main/java/cz/muni/fi/pv217/prociv/alerting/service/ReportingResource.java
@@ -7,6 +7,9 @@ import cz.muni.fi.pv217.prociv.alerting.service.services.ReportingService;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -28,6 +31,10 @@ public class ReportingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Counted(name = "numberOfNewReports", description = "How many new reports have been added.")
     @Timed(name = "newReportTimer", description = "A measure of how long it takes to add a new report.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Add a new report. Requires user JWT authorization token.")
+    @RequestBody(description = "Report info")
+    @APIResponse(responseCode = "200", description = "Successfully added a new report.")
+    @APIResponse(responseCode = "401", description = "Unauthorized access.")
     public String newReport(Report report, @Context SecurityContext ctx) {
         if (ctx != null && ctx.getUserPrincipal() != null) {
             report.username = ctx.getUserPrincipal().getName();
@@ -41,6 +48,8 @@ public class ReportingResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Find all reports.")
+    @APIResponse(responseCode = "200", description = "List of all reports.")
     public List<Report> getAllReports() {
         return reportingService.getAllReports();
     }
@@ -48,6 +57,8 @@ public class ReportingResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Find report with specific ID.")
+    @APIResponse(responseCode = "200", description = "Report with given ID found.")
     public Report getReport(@PathParam("id") Long id) throws AlertException {
         try {
             return reportingService.getReport(id);
@@ -61,6 +72,9 @@ public class ReportingResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Timed(name = "filterReportTimer", description = "A measure of how long it takes to filter reports.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Find all reports with given filter.")
+    @RequestBody(description = "Report filter")
+    @APIResponse(responseCode = "200", description = "List of all reports with given filter.")
     public List<Report> getFilteredReports(ReportFilterOptions options) {
         return reportingService.getFilteredReports(options.date, options.location);
     }
