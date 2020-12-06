2480c04b22e055245bacc91d1f0bad5a20480543
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-12-06 12:22:52 +0100

    auth service detailed openapi

--- auth-service/src/main/java/cz/muni/fi/pv217/prociv/auth/service/AuthenticationResource.java
+++ auth-service/src/main/java/cz/muni/fi/pv217/prociv/auth/service/AuthenticationResource.java
@@ -7,6 +7,10 @@ import cz.muni.fi.pv217.prociv.auth.service.services.TokenService;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -25,6 +29,10 @@ public class AuthenticationResource {
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Counted(name = "numberOfLogins", description = "How many logins have been performed.")
+    @Operation(summary = "Login with your credentials.")
+    @RequestBody(description = "Credentials")
+    @APIResponse(responseCode = "200", description = "Successful login, returns JWT token.")
+    @APIResponse(responseCode = "400", description = "Unsuccessful login.")
     public String login(AuthData data) throws AuthException {
         try {
             if (!authService.loginUser(data.username, data.password)) {
@@ -42,6 +50,10 @@ public class AuthenticationResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Counted(name = "numberOfRegistrations", description = "How many registrations have been performed.")
     @Timed(name = "registrationTimer", description = "A measure of how long it takes to register a new user.", unit = MetricUnits.MILLISECONDS)
+    @Operation(summary = "Registration of a new user.")
+    @RequestBody(description = "Credentials")
+    @APIResponse(responseCode = "200", description = "Successful registration.")
+    @APIResponse(responseCode = "400", description = "Unsuccessful registration.")
     public String register(AuthData data) throws AuthException {
         try {
             authService.registerUser(data.username, data.password);
