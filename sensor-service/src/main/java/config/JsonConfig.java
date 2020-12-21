2f074f57ce6ef711dd14a550b70c86497bc74983
jiri_siroky <legendcf@seznam.cz>
2020-12-21 11:15:31 +0100

    json-config

---
+++ sensor-service/src/main/java/config/JsonConfig.java
@@ -0,0 +1,22 @@
+package config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import java.text.SimpleDateFormat;
+
+@Provider
+public class JsonConfig implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonConfig() {
+        objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> aClass) {
+        return objectMapper;
+    }
+}
